a34f997e7d665027b41651c16a6aa2dbead606c1
jonasMartins0809 <jonasverdadeiromartins@gmail.com>
2019-12-20 02:40:50 -0300

    exercicio 1 e 2

---
+++ ex1/resolucao1.py
@@ -0,0 +1,9 @@
+palavra = str(input('Insira a palavra: '))
+lista = [palavra, palavra, palavra, palavra, palavra, palavra]
+i=1
+while i <= 7:
+    print(lista)
+    i = i + 1
+
+
+    
---
+++ ex2/resolucao2.py
@@ -0,0 +1,8 @@
+nome = str(input('insira sua palavra: '))
+nome = nome.replace('a','@')
+nome = nome.replace('e','&')
+nome = nome.replace('i','!')
+nome = nome.replace('u','*')a
+
+print (nome)
+
